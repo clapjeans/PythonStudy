f048e1abc468469e4dd4b2f0545e2ed12eef3664
Clapjeans <tnwls96101@daum.net>
2022-03-25 10:40:38 +0900

    폰번호

--- programas/participant.py
+++ programas/participant.py
@@ -1,15 +1,14 @@
 def solution(participant, completion):
-    
-    temp=0
-    dic={}
+    temp = 0
+    dic = {}
 
     for p in participant:
-        dic[hash(p)]=p
-        
+        dic[hash(p)] = p
+
         temp += int(hash(p))
-        
+
     for com in completion:
         temp -= hash(com)
-        
-    
+
     return dic[temp]
+
